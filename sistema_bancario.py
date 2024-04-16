f204cea675787fa300ae162475bab0e81bb2bdcc
Ramon Braga <140903176+ramon-braga@users.noreply.github.com>
2024-04-16 12:58:51 -0300

    mais de 1 cliente suportado

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,3 +1,5 @@
+# Funções
+
 def depositar(saldo, valor, extrato):
 
     if valor > 0:
@@ -46,43 +48,123 @@ def exibir_extrato(saldo, *, extrato):
         print(f"=> Saldo total: R$ {saldo:.2f}")
         print("=".center(41, "="))
 
+def menu_operacoes_em_conta(cliente):
+
+    menu = """
+
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+
+    => """
+
+    saldo = cliente["contas"][0]["saldo"]
+    limite = 500
+    extrato = cliente["contas"][0]["extrato"]
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+        opcao = input(menu)
+
+        if opcao == "d":
+            valor = float(input("Informe o valor para deposito: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
+
+            cliente["contas"][0]["saldo"] = saldo
+            cliente["contas"][0]["extrato"] = extrato
+        
+        elif opcao == "s":
+            if numero_saques == LIMITE_SAQUES:
+                print("Número máximo permitido para saques diários atingido.")
+            
+            else:
+                print(f"Saldo disponível: R$ {saldo:.2f}")
+                valor = float(input("\nInforme o valor para saque: "))
+
+                saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques)
+
+                cliente["contas"][0]["saldo"] = saldo
+                cliente["contas"][0]["extrato"] = extrato
+            
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato=extrato)
+        
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+def login(clientes):
+    cpf = int(input("Informe seu CPF: "))
+    nao_encontrou = True
+
+    for cliente in clientes:
+        if cliente["cpf"] == cpf:
+            nao_encontrou = False
+            menu_operacoes_em_conta(cliente)
+    
+    if nao_encontrou:
+        print("CPF não encontrado.")
+        
+def cadastrar(*, clientes):
+    nome = input("Nome: ")
+    data_nascimento = input("Data de nascimento (DD/MM/AAAA): ")
+    cpf = int(input("CPF: "))
+    logadouro = input("Logadouro: ")
+    bairro = input("Bairro: ")
+    cidade = input("Cidade: ")
+    uf = input("UF: ")
+
+    num_conta = len(clientes) + 1
+
+    clientes.append(
+        {
+            "nome": nome,
+            "data_nascimento": data_nascimento,
+            "cpf": cpf,
+            "endereco": {
+                "logadouro": logadouro,
+                "bairro": bairro,
+                "cidade": cidade,
+                "uf": uf
+            },
+            "contas": [
+                { "num_conta": num_conta, "agencia": "0001", "saldo": 0, "extrato": "" }
+            ]
+        }
+    )
+    return clientes
+
+# Início do programa
+
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    1. Login
+    2. Cadastrar
+    0. Sair
 
 => """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+clientes = []
 
 while True:
-    opcao = input(menu)
+    opcao = int(input(menu))
 
-    if opcao == "d":
-        valor = float(input("Informe o valor para deposito: "))
-        saldo, extrato = depositar(saldo, valor, extrato)
-    
-    elif opcao == "s":
-        if numero_saques == LIMITE_SAQUES:
-            print("Número máximo permitido para saques diários atingido.")
+    if opcao == 1:
+        login(clientes)
         
-        else:
-            print(f"Saldo disponível: R$ {saldo:.2f}")
-            valor = float(input("\nInforme o valor para saque: "))
+    elif opcao == 2:
+        clientes = cadastrar(clientes=clientes)
+            
+        print()
+        print(clientes)
+        print()
 
-            saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite, numero_saques=numero_saques)
-        
-    elif opcao == "e":
-        exibir_extrato(saldo, extrato=extrato)
-    
-    elif opcao == "q":
+    elif opcao == 0:
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Opção inválida.")
